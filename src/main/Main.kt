f8f0a88e2d3408d1f33bda0415ca2a3c88bf69a7
replaykim <replaykim123@gmail.com>
2018-06-02 19:05:07 +0900

    expression

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -1,9 +1,16 @@
 package main
 
+
+
+//함수 정의
 fun helloWorld() : String {
     return "Hello World!!!"
 }
 
+//함수 표현식
+fun helloWorldExpression() = "Hollo World"
+
 fun main(args : Array<String>) : Unit {
     println(helloWorld())
+    println(helloWorldExpression())
 }
