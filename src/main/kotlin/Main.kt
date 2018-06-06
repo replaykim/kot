6f618175b16928870596c22250c147fc97ac3a37
replaykim <replaykim123@gmail.com>
2018-06-06 17:47:19 +0900

    elvis

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,12 @@ package main
 
 //함수 정의
 fun helloWorld() : String {
+//    val cantnull :String = null;  // 컴파일 에러!
+    val elvis :String? = null;
+
+    val  size = elvis?.length;  //elvis 연산자를 붙여야함
+//    elvis.equals()
+
     val hello : String = "Hello World!!!"
     var helloVar = "hello World!!!"
 
